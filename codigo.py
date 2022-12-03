316223daed3c8ab74f9891492b660ed6f0b4e3d6
simonvaru <pennywise1540b@hotmail.com>
2022-12-03 18:05:47 -0300

    5° commit crear BBDD y borrar campos

--- codigo.py
+++ codigo.py
@@ -1,6 +1,36 @@
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
+from funciones import *
+
+
+
+###########################FUNCIONES######################################
+# def conexionBBDD():
+#     miConexion=sqlite3.connect('pild_infor_59/Usuarios')
+#     miCursor=miConexion.cursor()
+#     try:
+#         miCursor.execute('''
+#             CREATE TABLE DATAOUSUARIOS(
+#             ID INTEGER PRIMARY KEY AUTOINCREMENT,
+#             NOMBRE_USUARIO VARCHAR(50),
+#             PASSWORD VARCHAR(50),
+#             APELLIDO VARCHAR(10),
+#             DIRECCION VARCHAR(50),
+#             COMENTARIOS VARCHAR(100)) 
+#             ''')
+#         messagebox.showinfo("BBDD", "BBDD creada con exito")
+#     except:
+#         messagebox.showwarning("¡Atencion!", "La BBDD ya existe")
+        
+def salir_app():
+    valor=messagebox.askquestion("Salir", "¿Deseas salir de la aplicacion?")
+    if valor=="yes":
+        root.destroy()
+
+###########################################################################
+
+
 
 root=Tk()
 
@@ -11,12 +41,12 @@ root.config(menu=barraMenu, width=300, height=300)
 
 bbddMenu=Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="BBDD", menu=bbddMenu)
-bbddMenu.add_command(label="Conectar")
-bbddMenu.add_command(label="Salir")
+bbddMenu.add_command(label="Conectar", command=conexionBBDD)
+bbddMenu.add_command(label="Salir", command=salir_app)
 
 borrarMenu=Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
-borrarMenu.add_command(label="Borrar campos")
+borrarMenu.add_command(label="Borrar campos", command=limpiar_campos)
 
 crudMenu=Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="CRUD", menu=crudMenu)
@@ -31,24 +61,31 @@ ayudaMenu.add_command(label="Licencia")
 ayudaMenu.add_command(label="Acerac de...")
 
 ###########################################################################
+###########################################################################
 #################COMIENZO DE CAMPOS VACIOS X RELLENAR######################
 
 miFrame=Frame(root)
 miFrame.pack()
 
-cuadroID=Entry(miFrame)
+miId=StringVar()
+miNombre=StringVar()
+miApellido=StringVar()
+miPass=StringVar()
+miDireccion=StringVar()
+
+cuadroID=Entry(miFrame, textvariable=miId)
 cuadroID.grid(row=0, column=1,padx=10,pady=10)
 
-cuadroNOMBRE=Entry(miFrame)
+cuadroNOMBRE=Entry(miFrame, textvariable=miNombre)
 cuadroNOMBRE.grid(row=1, column=1,padx=10,pady=10)
 cuadroNOMBRE.config(fg="black", justify="right")
 
-cuadroPASS=Entry(miFrame)
-cuadroPASS.grid(row=2, column=1,padx=10,pady=10)
-cuadroPASS.config(show="*")
+cuadroAPELLIDO=Entry(miFrame, textvariable=miApellido)
+cuadroAPELLIDO.grid(row=2, column=1,padx=10,pady=10)
 
-cuadroAPELLIDO=Entry(miFrame)
-cuadroAPELLIDO.grid(row=3, column=1,padx=10,pady=10)
+cuadroPASS=Entry(miFrame, textvariable=miPass)
+cuadroPASS.grid(row=3, column=1,padx=10,pady=10)
+cuadroPASS.config(show="*")
 
 cuadroDIRECCION=Entry(miFrame)
 cuadroDIRECCION.grid(row=4, column=1,padx=10,pady=10)
@@ -59,9 +96,45 @@ scrollVert=Scrollbar(miFrame, command=textoCOMENTARIO.yview)
 scrollVert.grid(row=5, column=2,sticky="nsew")
 textoCOMENTARIO.config(yscrollcommand=scrollVert.set)
 
+#######################LABEL###############################################
 
+idLabel=Label(miFrame, text="Id: ")
+idLabel.grid(row=0, column=0, sticky="e", padx=10, pady="10")
 
+nombreLabel=Label(miFrame, text="Nombre: ")
+nombreLabel.grid(row=1, column=0, sticky="e", padx=10, pady="10")
 
+apellidoLabel=Label(miFrame, text="Apellido: ")
+apellidoLabel.grid(row=2, column=0, sticky="e", padx=10, pady="10")
+
+passLabel=Label(miFrame, text="Pass: ")
+passLabel.grid(row=3, column=0, sticky="e", padx=10, pady="10")
+
+direccionLabel=Label(miFrame, text="Direccion: ")
+direccionLabel.grid(row=4, column=0, sticky="e", padx=10, pady="10")
+
+comentariosLabel=Label(miFrame, text="Comentarios: ")
+comentariosLabel.grid(row=5, column=0, sticky="e", padx=10, pady="10")
+
+##########################################################################
+##########################################################################
+
+####################BOTONES INFERIORES####################################
+
+miFrame2=Frame(root)
+miFrame2.pack()
+
+botonCrear=Button(miFrame2, text="Create")
+botonCrear.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+botonLeer=Button(miFrame2, text="Leer")
+botonLeer.grid(row=0, column=1, sticky="e", padx=10, pady=10)
+
+botonActualizar=Button(miFrame2, text="Actualizar")
+botonActualizar.grid(row=0, column=2, sticky="e", padx=10, pady=10)
+
+botonBorrar=Button(miFrame2, text="Borrar")
+botonBorrar.grid(row=0, column=3, sticky="e", padx=10, pady=10)
 
 ###########################################################################
 root.mainloop()
---
+++ funciones.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+# from codigo import *
+
+
+
+def conexionBBDD():
+    miConexion=sqlite3.connect('pild_infor_59/Usuarios')
+    miCursor=miConexion.cursor()
+    try:
+        miCursor.execute('''
+            CREATE TABLE DATAOUSUARIOS(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NOMBRE_USUARIO VARCHAR(50),
+            PASSWORD VARCHAR(50),
+            APELLIDO VARCHAR(10),
+            DIRECCION VARCHAR(50),
+            COMENTARIOS VARCHAR(100)) 
+            ''')
+        messagebox.showinfo("BBDD", "BBDD creada con exito")
+    except:
+        messagebox.showwarning("¡Atencion!", "La BBDD ya existe")
+        
+
+def limpiar_campos():
+    miNombre.set("")
+    miId.set("")
+    miApellido.set("")
+    miDireccion.set("")
+    miPass.set("")
+    textoCOMENTARIO.delete(1.0, END)
+    
+    
+ 
